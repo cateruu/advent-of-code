7e83b8514276501ad8b2cece4ddd6411990d2d8e
cateruu <cater981pl@gmail.com>
2023-12-01 17:03:39 +0100

    day1: part 1

--- day1/main.go
+++ day1/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello, World!");
+	readFile, err := os.Open("input.txt");
+	defer readFile.Close()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fs := bufio.NewScanner(readFile)
+
+	sum := 0
+	for fs.Scan() {
+		numbers := []string{} 
+
+		for i := 0; i < len(fs.Text()); i++ {
+			if fs.Text()[i] >= '0' && fs.Text()[i] <= '9' {
+				numbers = append(numbers, string(fs.Text()[i]))
+			}
+		}
+
+		first := numbers[0]
+		last := numbers[len(numbers) - 1]
+		
+		number, _ := strconv.Atoi(first + last)
+
+		sum += number
+	}
+
+	fmt.Println(sum)
 }
