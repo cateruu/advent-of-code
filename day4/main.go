7ff48944b39d9e21b29bff74141f500bbdae7e02
cateruu <cater981pl@gmail.com>
2023-12-04 16:15:33 +0100

    day4: part 2

--- day4/main.go
+++ day4/main.go
@@ -9,6 +9,11 @@ import (
 	"time"
 )
 
+type Game struct {
+	Won           int
+	NumberOfCards int
+}
+
 func main() {
 	start := time.Now()
 
@@ -21,7 +26,7 @@ func main() {
 
 	fs := bufio.NewScanner(readFile)
 
-	sum := 0
+	allCards := []Game{}
 	for fs.Scan() {
 		game := strings.Split(fs.Text(), ":")[1]
 
@@ -29,25 +34,41 @@ func main() {
 		winningNumbers := strings.Split(numbers[0], " ")[1:]
 		ourNumbers := strings.Split(numbers[1], " ")[1:]
 
-		won := 0
-		for _, n := range ourNumbers {
-			if slices.Contains(winningNumbers, n) && n != "" {
-				won++
-			}
-		}
-
-		points := 0
-		for i := 1; i <= won; i++ {
-			if i == 1 {
-				points += 1
-			} else {
-				points *= 2
-			}
-		}
-
-		sum += points
+		won := checkWinNumbers(ourNumbers, winningNumbers)
+		allCards = append(allCards, Game{Won: won, NumberOfCards: 1})
 	}
 
+	for i, game := range allCards {
+		for j := 0; j < game.NumberOfCards; j++ {
+			playCopiedCards(&game, &allCards, game.Won, i)
+		}
+	}
+
+	sum := 0
+	for _, g := range allCards {
+		sum += g.NumberOfCards
+	}
 	elapsed := time.Since(start)
 	fmt.Println(sum, elapsed)
+}
+
+func checkWinNumbers(nums []string, winNums []string) int {
+	won := 0
+	for _, n := range nums {
+		if slices.Contains(winNums, n) && n != "" {
+			won++
+		}
+	}
+
+	return won
+}
+
+func playCopiedCards(currentCard *Game, allCards *[]Game, cardsLeft int, currentCardIdx int) {
+	if cardsLeft <= 0 || currentCardIdx+1 >= len(*allCards) {
+		return
+	}
+
+	(*allCards)[currentCardIdx+1].NumberOfCards++
+
+	playCopiedCards(currentCard, allCards, cardsLeft-1, currentCardIdx+1)
 }
