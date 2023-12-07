8a07b148f361f3eee62b698c433796b19766f98c
cateruu <cater981pl@gmail.com>
2023-12-07 16:00:15 +0100

    day7: part 1

---
+++ day7/go.mod
@@ -0,0 +1,5 @@
+module advent/day7
+
+go 1.21.4
+
+require github.com/icza/abcsort v0.0.0-20230330133725-d6ace6446f81 // indirect
---
+++ day7/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/icza/abcsort"
+)
+
+type Hand struct {
+	Hand string
+	Bid  int
+}
+
+func main() {
+	start := time.Now()
+	readFile, err := os.Open("input.txt")
+	defer readFile.Close()
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	fs := bufio.NewScanner(readFile)
+
+	handTypes := make(map[string][]Hand)
+	for fs.Scan() {
+		game := strings.Split(fs.Text(), " ")
+
+		handType := getHandType(game[0])
+		bid, _ := strconv.Atoi(game[1])
+		handTypes[handType] = append(handTypes[handType], Hand{Hand: game[0], Bid: bid})
+	}
+
+	sorter := abcsort.New("123456789TJQKA")
+	for _, hand := range handTypes {
+		sorter.Slice(hand, func(i int) string {
+			return hand[i].Hand
+		})
+	}
+
+	typeSlice := []string{"HighCard", "OnePair", "TwoPair", "ThreeOfKind", "FullHouse", "FourOfKind", "FiveOfKind"}
+
+	sum := 0
+	rank := 1
+	for _, handType := range typeSlice {
+		for _, hand := range handTypes[handType] {
+			sum += hand.Bid * rank
+			rank++
+		}
+	}
+
+	elapsed := time.Since(start)
+	fmt.Println(sum, elapsed)
+}
+
+func getHandType(hand string) string {
+	allCards := make(map[rune]int)
+	for _, card := range hand {
+		allCards[card]++
+	}
+
+	nums := []string{}
+	for _, card := range allCards {
+		nums = append(nums, fmt.Sprint(card))
+	}
+
+	sort.Strings(nums)
+	handValue := strings.Join(nums, "")
+
+	if handValue == "5" {
+		return "FiveOfKind"
+	} else if handValue == "14" {
+		return "FourOfKind"
+	} else if handValue == "23" {
+		return "FullHouse"
+	} else if handValue == "113" {
+		return "ThreeOfKind"
+	} else if handValue == "122" {
+		return "TwoPair"
+	} else if handValue == "1112" {
+		return "OnePair"
+	} else {
+		return "HighCard"
+	}
+}
